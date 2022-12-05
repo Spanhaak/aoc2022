fc64f41114c2dd5c6e7a54a553b18f54c017819f
mspanhaak <marco.spanhaak1@ingka.ikea.com>
2022-12-05 22:04:05 +0100

    feat: not finished with day 5, in the rabbit hole, need to dig out

--- day5/day5.py
+++ day5/day5.py
@@ -2,13 +2,24 @@
 
 from itertools import islice
 
+# First generate a list of stacks
+list_of_values = []
+
 with open("input.txt", 'r', encoding = 'utf-8') as f:
-    # give me the first 8 rows
+    values_line = [next(f) for x in range(9)]
+    values_line = values_line[-1]
+    for number in values_line:
+        if number == " " or number == '':
+            continue
+        else:
+            list_of_values.append(values_line.rfind(number))
+    
+# Create lists for holding the boxes
+with open("input.txt", 'r', encoding = 'utf-8') as f:
     head = [next(f) for x in range(8)]
 
-    # Get the letters of the stacks in a list
-    list_of_values = (1, 5, 9, 13, 17, 21, 25, 29, 33)
-
+    bunch_of_crates = {}
+    
     for value in list_of_values:
         temp_list=[]
         for line in head:
@@ -16,25 +27,39 @@ with open("input.txt", 'r', encoding = 'utf-8') as f:
             if entry == '' or entry == ' ':
                 continue
             temp_list.append(entry)
-        if value == 1 : list1 = value = temp_list[::-1]
-        if value == 5 : list2 = value = temp_list[::-1]
-        if value == 9 : list3 = value = temp_list[::-1]
-        if value == 13 : list4 = value = temp_list[::-1]
-        if value == 17 : list5 = value = temp_list[::-1]
-        if value == 21 : list6 = value = temp_list[::-1]
-        if value == 25 : list7 = value = temp_list[::-1]
-        if value == 29 : list8 = value = temp_list[::-1]
-        if value == 33 : list9 = value = temp_list[::-1]
+        if value == 1 : bunch_of_crates['1']=list1 = value = temp_list[::-1]
+        if value == 5 : bunch_of_crates['2']=list2 = value = temp_list[::-1] 
+        if value == 9 : bunch_of_crates['3']=list3 = value = temp_list[::-1]
+        if value == 13 : bunch_of_crates['4']=list4 = value = temp_list[::-1]
+        if value == 17 : bunch_of_crates['5']=list5 = value = temp_list[::-1]
+        if value == 21 : bunch_of_crates['6']=list6 = value = temp_list[::-1]
+        if value == 25 : bunch_of_crates['7']=list7 = value = temp_list[::-1]
+        if value == 29 : bunch_of_crates['8']=list8 = value = temp_list[::-1]
+        if value == 33 : bunch_of_crates['9']=list9 = value = temp_list[::-1]
 
-    # List have been created
-    # Give me the rest of the file
-    # Provide actions needed
-    #body = [next(f) for x in range(503)]
-    body = [next(f) for x in range(3)]
+    body = [next(f) for x in range(503)]
+
     for line in body:
         if line.startswith('move'):
-            print(line)
-            print(value)
+            line = line.replace('from ', '')
+            line = line.replace('to ', '')
+            line = line.replace('move ', '')
+            line = line.split(" ")
+            
+            from_dict   = line[1]
+            from_dict   = from_dict.rstrip()
+            to_dict     = line[2]
+            to_dict     = to_dict.rstrip()
+            boxes       = int(line[0])
+            print(boxes)
+        
+            selected_crates = (bunch_of_crates[from_dict][-boxes:])
+            for each_box in selected_crates: (bunch_of_crates[to_dict].append(each_box))
+            for each_box in selected_crates: (bunch_of_crates[from_dict].remove(each_box))   
 
 
-# WIP continue later
+    final = (bunch_of_crates['1'][0], bunch_of_crates['2'][0], bunch_of_crates['3'][0], bunch_of_crates['4'][0], bunch_of_crates['5'][0], bunch_of_crates['6'][0], bunch_of_crates['7'][0], bunch_of_crates['8'][0], bunch_of_crates['9'][0])
+    final = (' '.join(final))
+    final = final.replace(" ", '')
+    print(final)
+    # BWQZJNRLF   
