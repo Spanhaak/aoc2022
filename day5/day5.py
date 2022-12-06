775f30a420220afef2aeadc83f85571e8487eaef
mspanhaak <marco.spanhaak1@ingka.ikea.com>
2022-12-06 09:22:51 +0100

    feat: after revision and code review, fixes

--- day5/day5.py
+++ day5/day5.py
@@ -1,64 +1,52 @@
-''' https://adventofcode.com/2022/day/5 '''
-
-from itertools import islice
+""" https://adventofcode.com/2022/day/5 """
 
 # First generate stacks
 list_of_values = []
 
-with open("input.txt", 'r', encoding = 'utf-8') as f:
+with open("input.txt", "r", encoding="utf-8") as f:
     values_line = [next(f) for x in range(9)]
     values_line = values_line[-1]
     for number in values_line:
-        if number == " " or number == '':
+        if number == " " or number == "":
             continue
         else:
             list_of_values.append(values_line.rfind(number))
 
 # Create lists for holding the boxes
-with open("input.txt", 'r', encoding = 'utf-8') as f:
+with open("input.txt", "r", encoding="utf-8") as f:
     head = [next(f) for x in range(8)]
     bunch_of_crates = {}
-    
-    for value in list_of_values:
-        temp_list=[]
+
+    for i, value in enumerate(list_of_values):
+        temp_list = []
         for line in head:
-            entry=(line[value])
-            if entry == '' or entry == ' ':
+            entry = line[value]
+            if entry == "" or entry == " ":
                 continue
             temp_list.append(entry)
-        if value == 1 : bunch_of_crates['1']=list1 = value = temp_list[::-1]
-        if value == 5 : bunch_of_crates['2']=list2 = value = temp_list[::-1] 
-        if value == 9 : bunch_of_crates['3']=list3 = value = temp_list[::-1]
-        if value == 13 : bunch_of_crates['4']=list4 = value = temp_list[::-1]
-        if value == 17 : bunch_of_crates['5']=list5 = value = temp_list[::-1]
-        if value == 21 : bunch_of_crates['6']=list6 = value = temp_list[::-1]
-        if value == 25 : bunch_of_crates['7']=list7 = value = temp_list[::-1]
-        if value == 29 : bunch_of_crates['8']=list8 = value = temp_list[::-1]
-        if value == 33 : bunch_of_crates['9']=list9 = value = temp_list[::-1]
 
-        print(value)
+        bunch_of_crates[str(i + 1)] = temp_list[::-1]
 
-    body = f.readlines()[10:]
+    body = f.readlines()[2:]
 
     for line in body:
-        line = line.replace('from ', '')
-        line = line.replace('to ', '')
-        line = line.replace('move ', '')
+        line = line.replace("from ", "")
+        line = line.replace("to ", "")
+        line = line.replace("move ", "")
         line = line.split(" ")
-            
-        from_dict   = line[1]
-        from_dict   = from_dict.rstrip()
-        to_dict     = line[2]
-        to_dict     = to_dict.rstrip()
-        boxes       = int(line[0])
-        
-        selected_crates = (bunch_of_crates[from_dict][-boxes:])
-        for each_box in selected_crates: (bunch_of_crates[to_dict].append(each_box))
-        for each_box in selected_crates: (bunch_of_crates[from_dict].remove(each_box))
 
+        from_dict = line[1]
+        from_dict = from_dict.rstrip()
+        to_dict = line[2]
+        to_dict = to_dict.rstrip()
+        boxes = int(line[0])
 
-    final = (bunch_of_crates['1'][0], bunch_of_crates['2'][0], bunch_of_crates['3'][0], bunch_of_crates['4'][0], bunch_of_crates['5'][0], bunch_of_crates['6'][0], bunch_of_crates['7'][0], bunch_of_crates['8'][0], bunch_of_crates['9'][0])
-    final = (' '.join(final))
-    final = final.replace(" ", '')
+        selected_crates = bunch_of_crates[from_dict][-boxes:]
+        for each_box in reversed(selected_crates):
+            bunch_of_crates[to_dict].append(each_box)
+            bunch_of_crates[from_dict].pop()
+
+    final = "".join(v[-1] for v in bunch_of_crates.values())
     print(final)
-    # HNFFVRLCJ  
+
+    # HNFFVRLCJ
